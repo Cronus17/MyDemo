724ef3f89705e7b520c875155d8baa558728e719
Cronus17 <134943038+Cronus17@users.noreply.github.com>
2023-05-29 21:41:58 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+A=int(input('请输入A'))
+B=int(input('请输入B'))
+X=int(input('请输入X'))
+if A>1 and B==0:
+    X=X/A
+if A==2 or X>1:
+    X=X+1
+    print('A='+str(A),'B='+str(B),'X='+str(X))
+else:
+    print('A='+str(A),'B='+str(B),'X='+str(X))
+
+
